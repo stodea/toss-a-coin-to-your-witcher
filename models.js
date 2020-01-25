4e35a5c350facd10f1bca1805e7c8a53b922984d
stevenknieven <stefan.nicolae.todea@gmail.com>
2020-01-26 01:08:02 +0200

    updates

--- models.js
+++ models.js
@@ -2,5 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.model('User', new mongoose.Schema({
     email: String,
-    passwordHash: String
+    passwordHash: String,
+    subscriptionActive: { type: Boolean, defaul: false },
+    customerId: String,
+    subscriptionId: String
 }))
